3a8773a88fea3f47f8d3734dccb7e323a8309e24
Abazhiz <abdulazizrealme8@gmail.com>
2023-09-22 11:01:50 +0700

    Added new python file -> 3-Alur-Program-Python.py

---
+++ 3-Alur-Program-Python.py
@@ -0,0 +1,16 @@
+print('Hello')
+print('World')
+print('Hello World')
+
+print('Halo Bro')
+# ini adalah comment
+
+'''
+Ini adalah comment multiline
+'''
+
+# Cara pindahin 1 baris tekan tombol ALT + panah UP/DOWN
+
+# Kita bisa mengcompile python ke yang namanya ByteCode
+# Cara mengcompile, buka terminal & tuliskan
+# python -m py_compile [nama file].py
