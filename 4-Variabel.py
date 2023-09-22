ff4036f11f97daa5ec8e1d548ba13ea0509da543
Abazhiz <abdulazizrealme8@gmail.com>
2023-09-22 14:09:59 +0700

    added file 4-Variabel.py

---
+++ 4-Variabel.py
@@ -0,0 +1,28 @@
+ # Variabel adalah tempat menyimpan data
+
+# Menaru / Assignment Nilai
+a = 10
+x = 5
+panjang = 1000
+
+# pemanggilan pertama
+print('Nilai a =',a)
+print(f'Nilai x = {x}')
+print('Nilai panjang =',panjang)
+
+# Cara Copy 1 baris dengan ALT + SHIFT + UP/DOWN
+# Cara naikin 1 baris dengan ALT + UP/DOWN
+
+# Penamaan
+nilai_y = 15 # dengan menggunakan underscore
+juta10 = 10000000 # ini boleh
+nilaiZ = 17.5 # ini boleh
+
+# pemanggilan kedua
+print('Nilai a =', a)
+a = 7
+print('Nilai a =', a)
+
+# assignment indirect
+b = a
+print('Nilai b =', b)
